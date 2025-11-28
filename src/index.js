cc13617bfea6acba409d1b11a996da5b8181a02c
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-11-28 09:13:09 -0300

    atualizado urlencode e params<body

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const port = 3000;
 app.set('view engine', 'ejs')
 app.set('views',"./views")
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 require("../db/db")(app);
 require("../rotas/home")(app);
 require("../rotas/getnoticias")(app);
